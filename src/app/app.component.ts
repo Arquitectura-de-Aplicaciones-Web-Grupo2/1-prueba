8e37229420d1af60b8962dc3025d72ab4af3b111
Jhimy <u201818218@upc.edu.pe>
2022-09-30 20:49:24 -0500

    v1-crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import{Producto}from './models/producto';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '1-prueba';
+  productoArray: Producto[]=[
+    {id:1, name:"producto1",precio:10},
+    {id:2, name:"producto2",precio:20},
+    {id:3, name:"producto3",precio:30},
+    {id:4, name:"producto4",precio:40},
+  ]
 }
---
+++ src/app/models/producto.ts
@@ -0,0 +1,5 @@
+export class Producto {
+  id:number | undefined;
+  name:string| undefined;
+  precio:number| undefined;
+}
